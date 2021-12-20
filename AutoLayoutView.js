0f73bc7b2fe38d5e8907632c0faf0f64d75ad126
Talha Naqvi <talha.naqvi@shopify.com>
2021-12-20 21:25:41 +0530

    init

---
+++ AutoLayoutView.js
@@ -0,0 +1,3 @@
+import {requireNativeComponent} from 'react-native';
+const ALV = requireNativeComponent('AutoLayoutView');
+export default ALV;
---
+++ android/app/build/generated/source/buildConfig/debug/com/flatlistpro/BuildConfig.java
@@ -0,0 +1,12 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.flatlistpro;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.flatlistpro";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
