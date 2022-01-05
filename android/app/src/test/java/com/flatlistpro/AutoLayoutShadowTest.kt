ae40073799958053eff67b22b5129185acd9daf1
Talha Naqvi <talha.naqvi@shopify.com>
2022-01-05 14:20:02 -0800

    made code more test friendly

---
+++ android/app/src/test/java/com/flatlistpro/AutoLayoutShadowTest.kt
@@ -0,0 +1,13 @@
+package com.flatlistpro
+
+import org.junit.jupiter.api.Assertions.*
+
+import org.junit.jupiter.api.Test
+
+internal class AutoLayoutShadowTest {
+
+    @Test
+    fun clearGapsAndOverlaps() {
+
+    }
+}
