24dac49e960c7a76889aa97052991f5ff3bce1e5
Elvira Burchik <elviraburchik@gmail.com>
2022-01-14 16:06:19 +0100

    Updated isWithinBounds tests

--- ios/AutoLayoutViewTests.swift
+++ ios/AutoLayoutViewTests.swift
@@ -9,100 +9,101 @@ import XCTest
 @testable import FlatListPro
 
 class AutoLayoutViewTests: XCTestCase {
-    var autoLayoutView: AutoLayoutView?
+    var autoLayoutView = AutoLayoutView()
     let testCell = CellContainer()
     let cellHeight: CGFloat = 50
     let windowSize: CGFloat = 500
-    
-    override func setUpWithError() throws {
-        autoLayoutView = AutoLayoutView()
-    }
     
     func test_IsWithinBoundsVerticalListScrollToBottom() throws {
         testCell.frame = CGRect(x: 0, y: 0, width: cellHeight, height: cellHeight)
         print(testCell)
         // Top left corner of the screen
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 0,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 0,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize))
         
         // Partly visible
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 25,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 25,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize))
         
         // Above visible
-        XCTAssertFalse(autoLayoutView!.isWithinBounds(testCell,
-                                                      scrollOffset: 100,
-                                                      renderAheadOffset: 0,
-                                                      windowSize: windowSize))
+        XCTAssertFalse(autoLayoutView.isWithinBounds(testCell,
+                                                     scrollOffset: 100,
+                                                     renderAheadOffset: 0,
+                                                     windowSize: windowSize))
     }
     
     func test_IsWithinBoundsVerticalListScrollToTop() throws {
         testCell.frame = CGRect(x: 0, y: windowSize - cellHeight, width: cellHeight, height: cellHeight)
         // Bottom left corner of the screen
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 0,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 0,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize))
         
         // Partly visible
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: -25,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: -25,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize))
         
         // Below visible
-        XCTAssertFalse(autoLayoutView!.isWithinBounds(testCell,
-                                                      scrollOffset: -100,
-                                                      renderAheadOffset: 0,
-                                                      windowSize: windowSize))
+        XCTAssertFalse(autoLayoutView.isWithinBounds(testCell,
+                                                     scrollOffset: -100,
+                                                     renderAheadOffset: 0,
+                                                     windowSize: windowSize))
     }
     
     func test_IsWithinBoundsHorizontalListScrollToLeft() throws {
         testCell.frame = CGRect(x: 0, y: 0, width: cellHeight, height: cellHeight)
-        autoLayoutView!.setHorizontal(true)
         // Top left corner of the screen
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 0,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 0,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize,
+                                                    isHorizontal: true))
         
         // Partly visible
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 25,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 25,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize,
+                                                    isHorizontal: true))
         
         // Hidden
-        XCTAssertFalse(autoLayoutView!.isWithinBounds(testCell,
-                                                      scrollOffset: 100,
-                                                      renderAheadOffset: 0,
-                                                      windowSize: windowSize))
+        XCTAssertFalse(autoLayoutView.isWithinBounds(testCell,
+                                                     scrollOffset: 100,
+                                                     renderAheadOffset: 0,
+                                                     windowSize: windowSize,
+                                                     isHorizontal: true))
     }
     
     func test_IsWithinBoundsHorizontalListScrollToRight() throws {
         testCell.frame = CGRect(x: windowSize - cellHeight, y: 0, width: cellHeight, height: cellHeight)
-        autoLayoutView!.setHorizontal(true)
         // Bottom left corner of the screen
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: 0,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: 0,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize,
+                                                    isHorizontal: true))
         
         // Partly visible
-        XCTAssertTrue(autoLayoutView!.isWithinBounds(testCell,
-                                                     scrollOffset: -25,
-                                                     renderAheadOffset: 0,
-                                                     windowSize: windowSize))
+        XCTAssertTrue(autoLayoutView.isWithinBounds(testCell,
+                                                    scrollOffset: -25,
+                                                    renderAheadOffset: 0,
+                                                    windowSize: windowSize,
+                                                    isHorizontal: true))
         
-        // Below visible
-        XCTAssertFalse(autoLayoutView!.isWithinBounds(testCell,
-                                                      scrollOffset: -100,
-                                                      renderAheadOffset: 0,
-                                                      windowSize: windowSize))
+        testCell.frame = CGRect(x: windowSize + cellHeight, y: 0, width: cellHeight, height: cellHeight)
+        // Hidden
+        XCTAssertFalse(autoLayoutView.isWithinBounds(testCell,
+                                                     scrollOffset: 0,
+                                                     renderAheadOffset: 0,
+                                                     windowSize: windowSize,
+                                                     isHorizontal: true))
     }
     
 }
