3492d88d1d56e4951c7130ec74400bbf616ecdf6
Talha Naqvi <talha.naqvi@shopify.com>
2022-01-20 14:23:53 -0800

    Making data type as generic

--- src/RecyclerFlatList.tsx
+++ src/RecyclerFlatList.tsx
@@ -2,10 +2,10 @@ import React from "react";
 import {
   StyleProp,
   View,
-  ViewProps,
   ViewStyle,
   PixelRatio,
   RefreshControl,
+  FlatListProps
 } from "react-native";
 import {
   DataProvider,
@@ -19,36 +19,23 @@ import ItemContainer from "./CellContainer";
 import WrapperComponent from "./WrapperComponent";
 import invariant from 'invariant'
 
-export interface RecyclerFlatListProps extends ViewProps {
-  data: Array<any>;
+export interface RecyclerFlatListProps<T> extends FlatListProps<T> {
   estimatedHeight: number;
-  renderItem: any;
-  keyExtractor?: (data) => string;
-  ItemSeparatorComponent: React.ComponentType<any> | null | undefined;
-  numColumns: number;
-  inverted: boolean;
-  ListEmptyComponent: React.ComponentType<any> | null | undefined;
-  ListHeaderComponent: React.ComponentType<any> | null | undefined;
-  ListHeaderComponentStyle?: StyleProp<ViewStyle> | undefined | null;
-  ListFooterComponent: React.ComponentType<any> | null | undefined;
-  ListFooterComponentStyle?: StyleProp<ViewStyle> | undefined | null;
-  horizontal: boolean;
-  onEndReached?: () => void;
-  onEndReachedThreshold?: number | undefined;
-  onRefresh?: (() => void) | null | undefined;
-  refreshing?: boolean | undefined;
 }
 
-export interface RecyclerFlatListState {
+export interface RecyclerFlatListState<T> {
   dataProvider: DataProvider;
   numColumns: number;
   layoutProvider: LayoutProvider;
-  data: Array<any>;
+  data?: readonly T[] | null;
 }
 
-class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, RecyclerFlatListState> {
-  private _rowRenderer;
+class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, RecyclerFlatListState<T>> {
   private rlvRef?: RecyclerListView<RecyclerListViewProps, any>;
+
+  static props = {
+    data: []
+  }
 
   constructor(props) {
     super(props);
@@ -60,28 +47,27 @@ class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, Recycl
     const refreshingPrecondition = !(this.props.onRefresh && typeof this.props.refreshing !== 'boolean');
     const message = "Invariant Violation: `refreshing` prop must be set as a boolean in order to use `onRefresh`, but got `\"undefined\"`";
     invariant(refreshingPrecondition, message);
-    this._rowRenderer = this.rowRenderer.bind(this);
   }
 
   //Some of the state variables need to update when props change
-  static getDerivedStateFromProps(nextProps: RecyclerFlatListProps, prevState: RecyclerFlatListState): RecyclerFlatListState {
+  static getDerivedStateFromProps<T>(nextProps: RecyclerFlatListProps<T>, prevState: RecyclerFlatListState<T>): RecyclerFlatListState<T> {
     const newState = { ...prevState }
     if (newState.numColumns !== nextProps.numColumns) {
-      newState.numColumns = nextProps.numColumns > 0 ? nextProps.numColumns : 1;
+      newState.numColumns = nextProps.numColumns || 1;
       newState.layoutProvider = RecyclerFlatList.getLayoutProvider(newState.numColumns, () => nextProps.estimatedHeight);
     }
     if (nextProps.data !== prevState.data) {
       newState.data = nextProps.data;
-      newState.dataProvider = newState.dataProvider.cloneWithRows(nextProps.data)
+      newState.dataProvider = newState.dataProvider.cloneWithRows(nextProps.data as any[])
     }
     return newState;
   }
 
-  static getInitialState(props: RecyclerFlatListProps): RecyclerFlatListState {
-    const numColumns = props.numColumns > 0 ? props.numColumns : 1;
+  static getInitialState<T>(props: RecyclerFlatListProps<T>): RecyclerFlatListState<T> {
+    const numColumns = props.numColumns || 1;
     const sizeProvider = () => props.estimatedHeight;
     const dataProvider = new DataProvider((r1, r2) => { return r1 !== r2 });
-    return { numColumns, layoutProvider: RecyclerFlatList.getLayoutProvider(numColumns, sizeProvider), dataProvider: dataProvider.cloneWithRows(props.data), data: props.data };
+    return { numColumns, layoutProvider: RecyclerFlatList.getLayoutProvider(numColumns, sizeProvider), dataProvider: dataProvider.cloneWithRows(props.data as any[]), data: props.data };
   }
 
   //Using only grid layout provider as it can also act as a listview, sizeProvider is a function to support future overrides
@@ -101,7 +87,8 @@ class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, Recycl
   }
 
   onEndReached = () => {
-    this.props.onEndReached?.()
+    //known issue: RLV doesn't report distanceFromEnd
+    this.props.onEndReached?.({ distanceFromEnd: 0 })
   }
 
   footerComponent(props) {
@@ -143,16 +130,16 @@ class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, Recycl
           layoutProvider={this.state.layoutProvider}
           style={style as object}
           dataProvider={this.state.dataProvider}
-          rowRenderer={this._rowRenderer}
+          rowRenderer={this.rowRenderer}
           renderFooter={this.footerComponent(this.props)}
           canChangeSize={true}
-          isHorizontal={this.props.horizontal}
+          isHorizontal={!!this.props.horizontal}
           scrollViewProps={scrollViewProps}
           forceNonDeterministicRendering={true}
           renderItemContainer={this.renderItemContainer}
           renderContentContainer={this.renderContainer}
           onEndReached={this.onEndReached}
-          onEndReachedThreshold={this.props.onEndReachedThreshold}
+          onEndReachedThreshold={this.props.onEndReachedThreshold || undefined}
         />
       );
     }
@@ -190,7 +177,7 @@ class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, Recycl
     );
   };
 
-  rowRenderer(type, data, index) {
+  rowRenderer = (type, data, index) => {
     let header;
     if (index == 0 && this.props.ListHeaderComponent) {
       if (this.props.ListHeaderComponentStyle) {
@@ -204,7 +191,8 @@ class RecyclerFlatList extends React.PureComponent<RecyclerFlatListProps, Recycl
       }
     }
 
-    let elem = this.props.renderItem({ item: data });
+    //known issue: expected to pass separators which isn't available in RLV
+    let elem = this.props.renderItem?.({ item: data, index: index } as any);
     let elements = [header, elem];
     if (this.props.ItemSeparatorComponent) {
       elements.push(this.props.ItemSeparatorComponent);
