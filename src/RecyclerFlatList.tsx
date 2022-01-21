4cb40a923478256ca4f638cc27882b371776b805
Talha Naqvi <talha.naqvi@shopify.com>
2022-01-21 06:57:57 -0800

    reformat code

--- src/RecyclerFlatList.tsx
+++ src/RecyclerFlatList.tsx
@@ -1,23 +1,23 @@
-import React from "react";
+import React from 'react';
 import {
   StyleProp,
   View,
   ViewStyle,
   PixelRatio,
   RefreshControl,
-  FlatListProps
-} from "react-native";
+  FlatListProps,
+} from 'react-native';
 import {
   DataProvider,
   GridLayoutProvider,
   LayoutProvider,
   RecyclerListView,
   RecyclerListViewProps,
-} from "recyclerlistview";
-import AutoLayoutView from "./AutoLayoutView";
-import ItemContainer from "./CellContainer";
-import WrapperComponent from "./WrapperComponent";
-import invariant from 'invariant'
+} from 'recyclerlistview';
+import AutoLayoutView from './AutoLayoutView';
+import ItemContainer from './CellContainer';
+import WrapperComponent from './WrapperComponent';
+import invariant from 'invariant';
 
 export interface RecyclerFlatListProps<T> extends FlatListProps<T> {
   estimatedHeight: number;
@@ -30,57 +30,89 @@ export interface RecyclerFlatListState<T> {
   data?: readonly T[] | null;
 }
 
-class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, RecyclerFlatListState<T>> {
+class RecyclerFlatList<T> extends React.PureComponent<
+  RecyclerFlatListProps<T>,
+  RecyclerFlatListState<T>
+> {
   private rlvRef?: RecyclerListView<RecyclerListViewProps, any>;
 
   static props = {
-    data: []
-  }
+    data: [],
+  };
 
   constructor(props) {
     super(props);
     this.setup();
-    this.state = RecyclerFlatList.getInitialState(props)
+    this.state = RecyclerFlatList.getInitialState(props);
   }
 
   setup() {
-    const refreshingPrecondition = !(this.props.onRefresh && typeof this.props.refreshing !== 'boolean');
-    const message = "Invariant Violation: `refreshing` prop must be set as a boolean in order to use `onRefresh`, but got `\"undefined\"`";
+    const refreshingPrecondition = !(
+      this.props.onRefresh && typeof this.props.refreshing !== 'boolean'
+    );
+    const message =
+      'Invariant Violation: `refreshing` prop must be set as a boolean in order to use `onRefresh`, but got `"undefined"`';
     invariant(refreshingPrecondition, message);
   }
 
   //Some of the state variables need to update when props change
-  static getDerivedStateFromProps<T>(nextProps: RecyclerFlatListProps<T>, prevState: RecyclerFlatListState<T>): RecyclerFlatListState<T> {
-    const newState = { ...prevState }
+  static getDerivedStateFromProps<T>(
+    nextProps: RecyclerFlatListProps<T>,
+    prevState: RecyclerFlatListState<T>
+  ): RecyclerFlatListState<T> {
+    const newState = {...prevState};
     if (newState.numColumns !== nextProps.numColumns) {
       newState.numColumns = nextProps.numColumns || 1;
-      newState.layoutProvider = RecyclerFlatList.getLayoutProvider(newState.numColumns, () => nextProps.estimatedHeight);
+      newState.layoutProvider = RecyclerFlatList.getLayoutProvider(
+        newState.numColumns,
+        () => nextProps.estimatedHeight
+      );
     }
     if (nextProps.data !== prevState.data) {
       newState.data = nextProps.data;
-      newState.dataProvider = newState.dataProvider.cloneWithRows(nextProps.data as any[])
+      newState.dataProvider = newState.dataProvider.cloneWithRows(
+        nextProps.data as any[]
+      );
     }
     return newState;
   }
 
-  static getInitialState<T>(props: RecyclerFlatListProps<T>): RecyclerFlatListState<T> {
+  static getInitialState<T>(
+    props: RecyclerFlatListProps<T>
+  ): RecyclerFlatListState<T> {
     const numColumns = props.numColumns || 1;
     const sizeProvider = () => props.estimatedHeight;
-    const dataProvider = new DataProvider((r1, r2) => { return r1 !== r2 });
-    return { numColumns, layoutProvider: RecyclerFlatList.getLayoutProvider(numColumns, sizeProvider), dataProvider: dataProvider.cloneWithRows(props.data as any[]), data: props.data };
+    const dataProvider = new DataProvider((r1, r2) => {
+      return r1 !== r2;
+    });
+    return {
+      numColumns,
+      layoutProvider: RecyclerFlatList.getLayoutProvider(
+        numColumns,
+        sizeProvider
+      ),
+      dataProvider: dataProvider.cloneWithRows(props.data as any[]),
+      data: props.data,
+    };
   }
 
   //Using only grid layout provider as it can also act as a listview, sizeProvider is a function to support future overrides
-  static getLayoutProvider(numColumns: number, sizeProvider: (index) => number) {
+  static getLayoutProvider(
+    numColumns: number,
+    sizeProvider: (index) => number
+  ) {
     return new GridLayoutProvider(
-      numColumns,    //max span or, total columns
-      (index) => {   //type of the item for given index
+      numColumns, //max span or, total columns
+      (index) => {
+        //type of the item for given index
         return 0;
       },
-      (index) => {  //span of the item at given index, item can choose to span more than one column
+      (index) => {
+        //span of the item at given index, item can choose to span more than one column
         return 1;
       },
-      (index) => {  //estimated size of the item an given index
+      (index) => {
+        //estimated size of the item an given index
         return sizeProvider(index);
       }
     );
@@ -88,8 +120,8 @@ class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, 
 
   onEndReached = () => {
     //known issue: RLV doesn't report distanceFromEnd
-    this.props.onEndReached?.({ distanceFromEnd: 0 })
-  }
+    this.props.onEndReached?.({distanceFromEnd: 0});
+  };
 
   footerComponent(props) {
     return function () {
@@ -112,16 +144,18 @@ class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, 
     } else {
       let style = this.props.style ?? {};
       if (this.props.inverted === true) {
-        style = [style, { transform: [{ scaleY: -1 }] }]
+        style = [style, {transform: [{scaleY: -1}]}];
       }
 
-      let scrollViewProps: object = { style };
+      let scrollViewProps: object = {style};
       if (this.props.onRefresh) {
-        const refreshControl = (<RefreshControl
-          refreshing={this.props.refreshing as boolean}
-          onRefresh={this.props.onRefresh}
-        />);
-        scrollViewProps = { ...scrollViewProps, refreshControl: refreshControl }
+        const refreshControl = (
+          <RefreshControl
+            refreshing={this.props.refreshing as boolean}
+            onRefresh={this.props.onRefresh}
+          />
+        );
+        scrollViewProps = {...scrollViewProps, refreshControl: refreshControl};
       }
 
       return (
@@ -192,16 +226,16 @@ class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, 
     }
 
     //known issue: expected to pass separators which isn't available in RLV
-    let elem = this.props.renderItem?.({ item: data, index: index } as any);
+    let elem = this.props.renderItem?.({item: data, index: index} as any);
     let elements = [header, elem];
     if (this.props.ItemSeparatorComponent) {
       elements.push(this.props.ItemSeparatorComponent);
     }
 
-    let style: StyleProp<ViewStyle> = { flex: 1 };
+    let style: StyleProp<ViewStyle> = {flex: 1};
     if (this.props.inverted === true) {
       elements = elements.reverse();
-      style = [style, { transform: [{ scaleY: -1 }] }];
+      style = [style, {transform: [{scaleY: -1}]}];
     }
 
     return (
@@ -213,13 +247,13 @@ class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, 
         </>
       </View>
     );
-  }
+  };
 
   private recyclerRef = (ref: any) => {
-    this.rlvRef = ref
-  }
+    this.rlvRef = ref;
+  };
 
-  public scrollToEnd(params?: { animated?: boolean | null | undefined }) {
+  public scrollToEnd(params?: {animated?: boolean | null | undefined}) {
     this.rlvRef?.scrollToEnd(!!params?.animated);
   }
 
@@ -233,11 +267,18 @@ class RecyclerFlatList<T> extends React.PureComponent<RecyclerFlatListProps<T>, 
     this.rlvRef?.scrollToIndex(params.index, !!params.animated);
   }
 
-  public scrollToItem(params: { animated?: boolean | null | undefined; item: any; viewPosition?: number | undefined }) {
+  public scrollToItem(params: {
+    animated?: boolean | null | undefined;
+    item: any;
+    viewPosition?: number | undefined;
+  }) {
     this.rlvRef?.scrollToItem(params.item, !!params.animated);
   }
 
-  public scrollToOffset(params: { animated?: boolean | null | undefined; offset: number }) {
+  public scrollToOffset(params: {
+    animated?: boolean | null | undefined;
+    offset: number;
+  }) {
     const x = this.props.horizontal ? params.offset : 0;
     const y = this.props.horizontal ? 0 : params.offset;
     this.rlvRef?.scrollToOffset(x, y, !!params.animated);
