4f8e8fe1c4b0293d92d3369827c2cf52cabdf5a2
Marek Fořt <marek.fort@shopify.com>
2022-01-25 16:05:08 +0100

    WIP: Some changes

--- ios/Sources/BlankAreaView.swift
+++ ios/Sources/BlankAreaView.swift
@@ -54,40 +54,39 @@ import React
     
     override func layoutSubviews() {
         super.layoutSubviews()
-        if !didSet {
-            let scrollView = subviews.first!.subviews.first! as! UIScrollView
-            observation = scrollView.observe(\.contentOffset, changeHandler: { [weak self] scrollView, _ in
-                guard let self = self else { return }
-                // subviews.first!.subviews.first!.subviews.first!.subviews
-//                print(scrollView.contentOffset)
-                let scrollY = scrollView.contentOffset.y
-//                print("Scroll offset Native: \(scrollY)")
-                
-                let blank = self.computeBlankFromGivenOffset(scrollY)
-//                print("Blank offset: \(blank)")
-                
-                BlankAreaEventEmitter.INSTANCE?.onBlankArea(offset: blank) ?? assertionFailure("BlankAreaEventEmitter.INSTANCE was not initialized")
-            })
-            didSet = true
-        }
-        backgroundColor = .red
+        guard
+            !didSet,
+            let scrollView = subviews.first?.subviews.first as? UIScrollView
+        else { return }
+        observation = scrollView.observe(\.contentOffset, changeHandler: { [weak self] scrollView, _ in
+            guard let self = self else { return }
+            
+            let (startOffset, endOffset, blankArea) = self.computeBlankFromGivenOffset(for: scrollView)
+            
+            BlankAreaEventEmitter.INSTANCE?.onBlankArea(
+                startOffset: startOffset,
+                endOffset: endOffset,
+                blankArea: blankArea,
+                listSize: scrollView.frame.height
+            ) ?? assertionFailure("BlankAreaEventEmitter.INSTANCE was not initialized")
+        })
+        didSet = true
     }
     
-    func computeBlankFromGivenOffset(_ actualScrollOffset: CGFloat) -> CGFloat {
-        guard let scrollView = subviews.first?.subviews.first as? UIScrollView else { return 0 }
+    func computeBlankFromGivenOffset(for scrollView: UIScrollView) -> (CGFloat, CGFloat, CGFloat) {
         let cells = scrollView.subviews.first(where: { $0 is RCTScrollContentView })?.subviews ?? []
+        guard !cells.isEmpty else { return (0, 0, 0) }
+        
         guard
-            let firstCell = cells.first(where: { scrollViewContains($0, scrollOffset: actualScrollOffset) && $0.frame.size != .zero }),
-            let lastCell = cells.last(where: { scrollViewContains($0, scrollOffset: actualScrollOffset) && $0.frame.size != .zero })
+            let firstCell = cells.first(where: { scrollViewContains($0, scrollOffset: scrollView.contentOffset.y) && !$0.subviews.flatMap(\.subviews).isEmpty }),
+            let lastCell = cells.last(where: { scrollViewContains($0, scrollOffset: scrollView.contentOffset.y) && !$0.subviews.flatMap(\.subviews).isEmpty })
         else {
-            return cells.isEmpty ? 0 : scrollView.frame.height
+            return (scrollView.frame.height, scrollView.frame.height, scrollView.frame.height)
         }
-        if firstCell == lastCell {
-            return 0
-        }
-        let blankOffsetTop = firstCell.frame.minY - actualScrollOffset
-        let blankOffsetBottom = actualScrollOffset + scrollView.frame.height - lastCell.frame.maxY
-        return max(0, blankOffsetTop, blankOffsetBottom) // one of the values is negative, we look for the positive one
+        let blankOffsetTop = firstCell.frame.minY - scrollView.contentOffset.y
+        let blankOffsetBottom = scrollView.contentOffset.y + scrollView.frame.height - lastCell.frame.maxY
+        let blankArea = max(0, blankOffsetTop, blankOffsetBottom)
+        return (blankOffsetTop, blankOffsetBottom, blankArea)
     }
     
     func scrollViewContains(
@@ -98,11 +97,12 @@ import React
         let boundsStart = scrollOffset
         let boundsEnd = scrollOffset + scrollView.frame.height
         let cellFrame = cellView.frame
-        
-//        if isHorizontal {
-//            return (cellFrame.minX >= boundsStart || cellFrame.maxX >= boundsStart) && (cellFrame.minX <= boundsEnd || cellFrame.maxX <= boundsEnd)
-//        } else {
+  
+        let isHorizontal = scrollView.contentSize.width > scrollView.frame.width
+        if isHorizontal {
+            return (cellFrame.minX >= boundsStart || cellFrame.maxX >= boundsStart) && (cellFrame.minX <= boundsEnd || cellFrame.maxX <= boundsEnd)
+        } else {
             return (cellFrame.minY >= boundsStart || cellFrame.maxY >= boundsStart) && (cellFrame.minY <= boundsEnd || cellFrame.maxY <= boundsEnd)
-//        }
+        }
     }
 }
