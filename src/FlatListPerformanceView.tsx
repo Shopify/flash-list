ef9312117e4c00624c7fd5a7bb1dae98539983b1
Marek Fořt <marek.fort@shopify.com>
2022-02-08 11:19:22 +0100

    Fix lint issues

--- src/FlatListPerformanceView.tsx
+++ src/FlatListPerformanceView.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import { FlatListPerformanceViewNativeComponent } from "./FlatListPerformanceViewNativeComponent";
 
 const FlatListPerformanceView = ({ children }) => {
--- src/RecyclerFlatListPerformanceView.tsx
+++ src/RecyclerFlatListPerformanceView.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+
 import { RecyclerFlatListPerformanceViewNativeComponent } from "./RecyclerFlatListPerformanceViewNativeComponent";
 
 const RecyclerFlatListPerformanceView = ({ children }) => {
