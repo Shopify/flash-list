d74131dbeb1c382cb0054d90b12cab33ed5f81a1
Marek Fořt <marek.fort@shopify.com>
2022-03-15 10:04:09 +0100

    Add comments

--- android/src/main/kotlin/com/shopify/reactnative/flash_list/AutoLayoutView.kt
+++ android/src/main/kotlin/com/shopify/reactnative/flash_list/AutoLayoutView.kt
@@ -50,6 +50,7 @@ class AutoLayoutView(context: Context) : ReactViewGroup(context) {
     /** Sorts views by index and then invokes clearGaps which does the correction.
      * Performance: Sort is needed. Given relatively low number of views in RecyclerListView render tree this should be a non issue.*/
     private fun fixLayout() {
+        // Fixing layout during animation can interfere with it.
         if (animation != null) {
             return
         }
