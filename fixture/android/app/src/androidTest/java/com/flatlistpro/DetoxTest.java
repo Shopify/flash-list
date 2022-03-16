2a0a4e839e7fcfb5c5870dbcc459747a57422d11
Elvira Burchik <elviraburchik@gmail.com>
2022-03-16 16:49:49 +0100

    Fix android test launch

--- fixture/android/app/src/androidTest/java/com/flatlistpro/DetoxTest.java
+++ fixture/android/app/src/androidTest/java/com/flatlistpro/DetoxTest.java
@@ -22,9 +22,9 @@ public class DetoxTest {
     @Test
     public void runDetoxTests() {
         DetoxConfig detoxConfig = new DetoxConfig();
-        detoxConfig.idlePolicyConfig.masterTimeoutSec = 60;
+        detoxConfig.idlePolicyConfig.masterTimeoutSec = 90;
         detoxConfig.idlePolicyConfig.idleResourceTimeoutSec = 60;
-        detoxConfig.rnContextLoadTimeoutSec = 60;
+        detoxConfig.rnContextLoadTimeoutSec = 180;
 
         Detox.runTests(mActivityRule, detoxConfig);
     }
