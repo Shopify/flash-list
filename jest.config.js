6d314b2db7ae9f53a1dd1c75b07377157fe67e39
Marek Fořt <marek.fort@shopify.com>
2022-03-18 11:43:53 +0100

    Simplify jest config

--- jest.config.js
+++ jest.config.js
@@ -1,12 +1,9 @@
-const path = require("path");
-
 module.exports = {
   verbose: true,
   preset: "react-native",
   testEnvironment: "node",
   testMatch: ["<rootDir>/src/**/__tests__/**/*.(test|spec).(ts|tsx|js)"],
-  testPathIgnorePatterns: ["<rootDir>/node_modules/"],
   transformIgnorePatterns: [
-    "node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)",
+    "<rootDir>/node_modules/(?!((jest-)?react-native|@react-native(-community)?)/)",
   ],
 };
