b39b7cbed6a7b1167c2a78fe7c21da6a15689140
Marek Fořt <marek.fort@shopify.com>
2022-03-28 11:56:31 +0200

    Change e2e test to use Typescript

--- fixture/e2e/Twitter.test.e2e.ts
+++ fixture/e2e/Twitter.test.e2e.ts
@@ -1,18 +1,7 @@
-import * as path from "path";
-import * as fs from "fs";
-import { Platform } from "react-native";
-import {
-  wipeArtifactsLocation,
-  saveReference,
-  reference,
-} from "../src/Detox/SnapshotLocation";
+import { assertSnapshotsEqual, assertSnapshot } from "./utils/SnapshotAsserts";
+import { wipeArtifactsLocation, reference } from "./utils/SnapshotLocation";
 
-import {
-  assertSnapshotsEqual,
-  assertSnapshot,
-} from "../src/Detox/SnapshotAsserts";
-
-describe("FlashList", () => {
+describe("Twitter", () => {
   const flashListReferenceTestName = "Twitter with FlashList looks the same";
 
   beforeAll(async () => {
@@ -24,7 +13,7 @@ describe("FlashList", () => {
     await device.reloadReactNative();
   });
 
-  it("Twitter with FlashList looks the same", async () => {
+  it("with FlashList looks the same", async () => {
     await element(by.id("Twitter Timeline")).tap();
 
     const testRunScreenshotPath = await element(
@@ -34,7 +23,7 @@ describe("FlashList", () => {
     assertSnapshot(testRunScreenshotPath, flashListReferenceTestName);
   });
 
-  it("Twitter with FlatList looks the same as with FlashList", async () => {
+  it("with FlatList looks the same as with FlashList", async () => {
     const testName = "Twitter with FlatList looks the same as with FlashList";
 
     await element(by.id("Twitter FlatList Timeline")).tap();
