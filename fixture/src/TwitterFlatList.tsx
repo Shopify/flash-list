2b27bb262ac9e7510083d31d92e68f7e2b523025
Elvira Burchik <elviraburchik@gmail.com>
2022-03-29 10:11:38 +0200

    e2e: test orientation change (#241)
    * code clean up and added tests
    
    * tophatted and removed unnecessary code
    
    * Add test case for orientation change
    
    * Fix lint issue
    
    * Fix lint issue
    
    Co-authored-by: Talha Naqvi <talha.naqvi@shopify.com>

--- fixture/src/TwitterFlatList.tsx
+++ fixture/src/TwitterFlatList.tsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { FlatList, View } from "react-native";
+import { FlatList } from "react-native";
 import { FlatListPerformanceView } from "@shopify/react-native-performance-lists-profiler";
 
 import { tweets } from "./data/tweets";
@@ -9,21 +9,20 @@ import { Header, Footer, Divider } from "./Twitter";
 const TwitterFlatList = () => {
   return (
     <FlatListPerformanceView listName="TwitterFlatList">
-      <View testID="FlatList">
-        <FlatList
-          keyExtractor={(item) => {
-            return item.id;
-          }}
-          renderItem={({ item }) => {
-            return <TweetCell tweet={item} />;
-          }}
-          ListHeaderComponent={Header}
-          ListHeaderComponentStyle={{ backgroundColor: "#ccc" }}
-          ListFooterComponent={Footer}
-          ItemSeparatorComponent={Divider}
-          data={tweets}
-        />
-      </View>
+      <FlatList
+        testID="FlatList"
+        keyExtractor={(item) => {
+          return item.id;
+        }}
+        renderItem={({ item }) => {
+          return <TweetCell tweet={item} />;
+        }}
+        ListHeaderComponent={Header}
+        ListHeaderComponentStyle={{ backgroundColor: "#ccc" }}
+        ListFooterComponent={Footer}
+        ItemSeparatorComponent={Divider}
+        data={tweets}
+      />
     </FlatListPerformanceView>
   );
 };
