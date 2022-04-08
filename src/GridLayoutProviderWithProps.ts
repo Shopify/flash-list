545cb899a7896a33e177606e173a076cd628c439
Talha Naqvi <talha.naqvi@shopify.com>
2022-04-08 08:39:30 -0700

    fix typo

--- src/GridLayoutProviderWithProps.ts
+++ src/GridLayoutProviderWithProps.ts
@@ -68,7 +68,7 @@ export default class GridLayoutProviderWithProps<T> extends GridLayoutProvider {
     const layout = this.getLayoutManager()?.getLayouts()[index];
     if (layout) {
       // For the same index we can now return different estimates because average is updated in realtime
-      // Marking the layout as overriden will help layout manager avoid using the average after initial measurement
+      // Marking the layout as overridden will help layout manager avoid using the average after initial measurement
       layout.isOverridden = true;
       this.averageWindow.addValue(
         this.props.horizontal ? layout.width : layout.height
