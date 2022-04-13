3026ab02351daecf325be4475d6f0e110048ed51
Talha Naqvi <talha.naqvi@shopify.com>
2022-04-13 15:13:17 -0700

    lint fix

--- fixture/src/twitter/TwitterBenchmark.tsx
+++ fixture/src/twitter/TwitterBenchmark.tsx
@@ -1,10 +1,13 @@
 import React, { useRef } from "react";
 import { useBenchmark } from "@shopify/flash-list";
+
 import Twitter from "./Twitter";
+
 const TwitterBenchmark = () => {
   const ref = useRef();
   const [blankAreaTracker] = useBenchmark(ref, (res) => {
     if (!res.interrupted) {
+      // eslint-disable-next-line no-alert
       alert(res.formattedString);
     }
   });
