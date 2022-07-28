33e5088b84cc8932383fdc2ed046e9cbf2eaa077
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-07-28 13:44:36 +0200

    Update app

--- fabricfixture/src/App.tsx
+++ fabricfixture/src/App.tsx
@@ -2,9 +2,11 @@
  Use this component inside your React Native Application.
  A scrollable list with different item type
  */
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
-import {FlashList} from '@shopify/flash-list';
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { FlashList } from "@shopify/flash-list";
+
+import TwitterBenchmark from "./twitter/TwitterBenchmark";
 
 export interface PaginatedListState {
   // This property is used but eslint fails to find its usage.
@@ -15,11 +17,13 @@ export interface PaginatedListState {
 /** *
  * To test out just copy this component and render in you root component
  */
-export default class PaginatedList extends React.Component<PaginatedListState> {
+
+export default TwitterBenchmark;
+export class PaginatedList extends React.Component<PaginatedListState> {
   state: PaginatedListState = this.getInitialState();
 
   private getInitialState() {
-    return {elems: this._generateArray(0, 20)} as PaginatedListState;
+    return { elems: this._generateArray(0, 20) } as PaginatedListState;
   }
 
   private _generateArray(start: number, size: number) {
@@ -36,10 +40,10 @@ export default class PaginatedList extends React.Component<PaginatedListState> {
         keyExtractor={(item: number) => {
           return item.toString();
         }}
-        renderItem={({item}: {item: number}) => {
-          const backgroundColor = item % 2 === 0 ? '#00a1f1' : '#ffbb00';
+        renderItem={({ item }: { item: number }) => {
+          const backgroundColor = item % 2 === 0 ? "#00a1f1" : "#ffbb00";
           return (
-            <View style={{...styles.container, backgroundColor}}>
+            <View style={{ ...styles.container, backgroundColor }}>
               <Text>Cell Id: {item}</Text>
             </View>
           );
@@ -50,7 +54,7 @@ export default class PaginatedList extends React.Component<PaginatedListState> {
           const elems = [...this.state.elems];
           elems.push(...this._generateArray(elems.length, 20));
           this.setState(() => {
-            return {elems};
+            return { elems };
           });
         }}
         onEndReachedThreshold={0.2}
@@ -62,9 +66,9 @@ export default class PaginatedList extends React.Component<PaginatedListState> {
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    justifyContent: "space-around",
+    alignItems: "center",
     height: 120,
-    backgroundColor: '#00a1f1',
+    backgroundColor: "#00a1f1",
   },
 });
