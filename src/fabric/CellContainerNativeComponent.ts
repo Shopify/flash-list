e25181ce8f9b42c7ab8633d79a0af6a52ac4d7df
Jakub Piasecki <jakub.piasecki@swmansion.com>
2022-08-30 12:12:09 +0200

    Update codegen specs to use only TS

--- src/fabric/CellContainerNativeComponent.ts
+++ src/fabric/CellContainerNativeComponent.ts
@@ -1,16 +1,6 @@
-// @ts-ignore TODO: remove once there is a .d.ts file with definitions
-import codegenNativeComponentUntyped from "react-native/Libraries/Utilities/codegenNativeComponent";
-// @ts-ignore TODO: remove once there is a .d.ts file with definitions
-import type {
-  Int32,
-  // @ts-ignore TODO: remove once there is a .d.ts file with definitions
-} from "react-native/Libraries/Types/CodegenTypes";
-import type { ViewProps, HostComponent } from "react-native";
-
-// eslint-disable-next-line @typescript-eslint/ban-types
-const codegenNativeComponent = codegenNativeComponentUntyped as <T extends {}>(
-  name: string
-) => HostComponent<T>;
+import codegenNativeComponent from "react-native/Libraries/Utilities/codegenNativeComponent";
+import type { Int32 } from "react-native/Libraries/Types/CodegenTypes";
+import type { ViewProps } from "react-native";
 
 interface NativeProps extends ViewProps {
   index?: Int32;
