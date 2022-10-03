764aab319fc1c1bbbb4fd84c63b3eb48e1d0aa0a
Talha Naqvi <talha.naqvi@shopify.com>
2022-10-03 14:07:33 -0700

    Added tests

--- src/utils/ContentContainerUtils.ts
+++ src/utils/ContentContainerUtils.ts
@@ -42,7 +42,7 @@ export const updateContentStyle = (
 };
 
 export const hasUnsupportedKeysInContentContainerStyle = (
-  contentContainerStyleSource: ContentStyle | undefined
+  contentContainerStyleSource: ViewStyle | undefined
 ) => {
   const {
     paddingTop,
