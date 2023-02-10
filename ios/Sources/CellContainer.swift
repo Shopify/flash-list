711dd4fa3d3459df7926bd8b54f7a535bfda14e2
Daniel Friyia JR <daniel.friyia@shopify.com>
2023-02-10 09:17:15 -0500

    [DF] feat(#547): `experimentalMaintainTopContentPosition` works vertically

--- ios/Sources/CellContainer.swift
+++ ios/Sources/CellContainer.swift
@@ -2,8 +2,13 @@ import Foundation
 
 @objc class CellContainer: UIView {
     var index: Int = -1
+    var layoutType: String = ""
     
     @objc func setIndex(_ index: Int) {
         self.index = index
+    }
+  
+    @objc func setType(_ layoutType: String) {
+        self.layoutType = layoutType
     }
 }
