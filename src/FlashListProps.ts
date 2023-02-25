1b8a62a169dbbde0b06c044b0fbf26c39eae724a
Daniel Friyia JR <daniel.friyia@shopify.com>
2023-02-25 11:01:32 -0500

    [DF] feat(#547): update documentation

--- src/FlashListProps.ts
+++ src/FlashListProps.ts
@@ -335,8 +335,9 @@ export interface FlashListProps<TItem> extends ScrollViewProps {
 
   /**
    * If enabled, FlashList will try and maintain the position of the list when items are added from the top.
-   * This prop requires you define a `keyExtractor` function. The keyExtractor is used to compute the list
-   * top anchor. Without it, the list will fail to render.
+   * This prop requires you define a `keyExtractor` function. The `keyExtractor` is used to compute the list
+   * top anchor. Without it, the list will fail to render. If in debug mode, you may see flashes if new data
+   * comes in quickly. If this happens, please confirm you see this in release mode before reporting an issue.
    */
   experimentalMaintainTopContentPosition?: boolean;
 }
