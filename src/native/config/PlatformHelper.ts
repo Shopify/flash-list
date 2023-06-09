e2854a53c1d45ab2e636f15616ffbf3676c675fc
Sherif Mohamed <32880766+SherifMega@users.noreply.github.com>
2023-06-09 20:25:16 +0300

    fix inverted horizontal list performance laggy (#851)

--- src/native/config/PlatformHelper.ts
+++ src/native/config/PlatformHelper.ts
@@ -4,6 +4,7 @@ import { DefaultJSItemAnimator } from "recyclerlistview/dist/reactnative/platfor
 const PlatformConfig = {
   defaultDrawDistance: 250,
   invertedTransformStyle: { transform: [{ scaleY: -1 }] },
+  invertedTransformStyleHorizontal: { transform: [{ scaleX: -1 }] },
 };
 const getCellContainerPlatformStyles = (
   inverted: boolean,
--- src/native/config/PlatformHelper.web.ts
+++ src/native/config/PlatformHelper.web.ts
@@ -6,6 +6,7 @@ import { DefaultJSItemAnimator } from "recyclerlistview/dist/reactnative/platfor
 const PlatformConfig = {
   defaultDrawDistance: 2000,
   invertedTransformStyle: { transform: [{ scaleY: -1 }] },
+  invertedTransformStyleHorizontal: { transform: [{ scaleX: -1 }] },
 };
 const getCellContainerPlatformStyles = (
   inverted: boolean,
