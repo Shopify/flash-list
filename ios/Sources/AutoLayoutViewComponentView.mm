c12786fdc7050c88848878918474b98772a5a8f3
Jakub Piasecki <jakub.piasecki@swmansion.com>
2024-06-07 12:54:00 +0200

    Implement `onBlankAreaEvent` on iOS

--- ios/Sources/AutoLayoutViewComponentView.mm
+++ ios/Sources/AutoLayoutViewComponentView.mm
@@ -1,6 +1,7 @@
 #ifdef RCT_NEW_ARCH_ENABLED
 #import "AutoLayoutViewComponentView.h"
 #import <React/RCTConversions.h>
+#import <React/RCTViewManager.h>
 
 #import <react/renderer/components/rnflashlist/ComponentDescriptors.h>
 #import <react/renderer/components/rnflashlist/EventEmitters.h>
@@ -37,6 +38,18 @@ using namespace facebook::react;
     _autoLayoutView.userInteractionEnabled = false;
 
     self.contentView = _autoLayoutView;
+      
+    __weak AutoLayoutViewComponentView* weakSelf = self;
+    _autoLayoutView.onBlankAreaEventHandler = ^(CGFloat start, CGFloat end) {
+      AutoLayoutViewComponentView *strongSelf = weakSelf;
+      if (strongSelf != nullptr && strongSelf->_eventEmitter != nullptr) {
+        std::dynamic_pointer_cast<const facebook::react::AutoLayoutViewEventEmitter>(strongSelf->_eventEmitter)
+          ->onBlankAreaEvent(facebook::react::AutoLayoutViewEventEmitter::OnBlankAreaEvent{
+            .offsetStart = (int) floor(start),
+            .offsetEnd = (int) floor(end),
+        });
+      }
+    };
   }
 
   return self;
