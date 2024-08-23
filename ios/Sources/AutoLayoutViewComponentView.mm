63b934920306f2f0ad69e3926d76d0e6e08b68e9
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2024-08-23 10:52:12 +0200

    feat: add load method

--- ios/Sources/AutoLayoutViewComponentView.mm
+++ ios/Sources/AutoLayoutViewComponentView.mm
@@ -26,6 +26,12 @@ using namespace facebook::react;
     AutoLayoutView *_autoLayoutView;
 }
 
+// Needed because of this: https://github.com/facebook/react-native/pull/37274
++ (void)load
+{
+  [super load];
+}
+
 - (instancetype)initWithFrame:(CGRect)frame
 {
   if (self = [super initWithFrame:frame]) {
