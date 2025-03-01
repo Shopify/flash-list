3e25ea698dd59ba8e9a007bd58a2421aea196358
Talha Naqvi <naqvitalha@gmail.com>
2025-02-28 17:42:07 -0800

    Add grid sample

--- fixture/react-native/src/ExamplesScreen.tsx
+++ fixture/react-native/src/ExamplesScreen.tsx
@@ -21,6 +21,7 @@ export const ExamplesScreen = () => {
 
   const data: ExampleItem[] = [
     { title: "List", destination: "List" },
+    { title: "Grid", destination: "Grid" },
     { title: "SectionList", destination: "SectionList" },
     { title: "PaginatedList", destination: "PaginatedList" },
     { title: "Reminders", destination: "Reminders" },
