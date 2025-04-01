4d0ce3de7689347fffe03668dba95b0f6c04deba
Talha Naqvi <naqvitalha@gmail.com>
2025-04-01 20:28:59 +0530

    remove unnecessary logic

--- fixture/react-native/index.js
+++ fixture/react-native/index.js
@@ -6,9 +6,6 @@ import { AppRegistry, I18nManager } from "react-native";
 
 import App from "./src/App";
 import { name as appName } from "./app.json";
-import { enableNewCore } from "@shopify/flash-list/dist/enableNewCore";
-
-enableNewCore(true);
 
 I18nManager.forceRTL(false);
 
