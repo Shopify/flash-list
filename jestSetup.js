b0dd7e0362e8e58a9549fe0029d016b46aa3b9f9
Talha Naqvi <naqvitalha@gmail.com>
2025-05-14 08:51:49 -0700

    export recyclerview as default FlashList for tests

--- jestSetup.js
+++ jestSetup.js
@@ -1,17 +1,9 @@
 jest.mock("@shopify/flash-list", () => {
-  const ActualFlashList = jest.requireActual("@shopify/flash-list").FlashList;
-  class MockFlashList extends ActualFlashList {
-    componentDidMount() {
-      super.componentDidMount();
-      this.rlvRef?._scrollComponent?._scrollViewRef?.props.onLayout({
-        nativeEvent: { layout: { height: 900, width: 400 } },
-      });
-    }
-  }
+  const RecyclerView = jest.requireActual("@shopify/flash-list").RecyclerView;
+
   return {
     ...jest.requireActual("@shopify/flash-list"),
-    FlashList: MockFlashList,
-    AnimatedFlashList: MockFlashList,
+    FlashList: RecyclerView,
   };
 });
 
