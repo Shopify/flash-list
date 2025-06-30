91a19543e61b1231a367b744ed4d72ceaaee3ac8
Talha Naqvi <naqvitalha@gmail.com>
2025-06-30 23:28:11 +0530

    Remove deprecated props

--- src/recyclerview/RecyclerViewProps.ts
+++ src/recyclerview/RecyclerViewProps.ts
@@ -1,11 +1,1 @@
-import { ScrollViewProps } from "react-native";
-
-import { FlashListProps } from "../FlashListProps";
-
-export interface RecyclerViewProps<TItem>
-  extends Omit<FlashListProps<TItem>, "contentContainerStyle"> {
-  /**
-   * Style for the RecyclerView's parent container.
-   */
-  contentContainerStyle?: ScrollViewProps["contentContainerStyle"];
-}
+export { FlashListProps as RecyclerViewProps } from "../FlashListProps";
