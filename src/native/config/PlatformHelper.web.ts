f9c635a9080eacaa65e5a8bff57abc5434f16acb
Talha Naqvi <naqvitalha@gmail.com>
2025-07-01 17:48:56 +0530

    remove recyclerlistview

--- src/native/config/PlatformHelper.web.ts
+++ src/native/config/PlatformHelper.web.ts
@@ -1,36 +1,7 @@
-import React from "react";
-import { View } from "react-native";
-import { BaseItemAnimator } from "recyclerlistview";
-import { DefaultJSItemAnimator } from "recyclerlistview/dist/reactnative/platform/reactnative/itemanimators/defaultjsanimator/DefaultJSItemAnimator";
-
 const PlatformConfig = {
   defaultDrawDistance: 500,
   supportsOffsetCorrection: false,
   trackAverageRenderTimeForOffsetProjection: false,
-  invertedTransformStyle: { transform: [{ scaleY: -1 }] },
-  invertedTransformStyleHorizontal: { transform: [{ scaleX: -1 }] },
-};
-const getCellContainerPlatformStyles = (
-  inverted: boolean,
-  parentProps: { x: number; y: number; isHorizontal?: boolean }
-): { transform: string; WebkitTransform: string } | undefined => {
-  const transformValue = `translate(${parentProps.x}px,${parentProps.y}px)${
-    inverted ? ` ${parentProps.isHorizontal ? `scaleX` : `scaleY`}(-1)` : ``
-  }`;
-  return { transform: transformValue, WebkitTransform: transformValue };
 };
 
-const getItemAnimator = (): BaseItemAnimator | undefined => {
-  return new DefaultJSItemAnimator();
-};
-
-const getFooterContainer = (): React.ComponentClass | undefined => {
-  return View;
-};
-
-export {
-  PlatformConfig,
-  getCellContainerPlatformStyles,
-  getItemAnimator,
-  getFooterContainer,
-};
+export { PlatformConfig };
