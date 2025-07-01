4fb809967fe46a9f32a1ba98d8a7d64a2aa09148
Talha Naqvi <naqvitalha@gmail.com>
2025-07-01 17:48:56 +0530

    remove recyclerlistview

--- src/native/config/PlatformHelper.ios.ts
+++ src/native/config/PlatformHelper.ios.ts
@@ -1,30 +1,7 @@
-import { BaseItemAnimator } from "recyclerlistview";
-
 const PlatformConfig = {
   defaultDrawDistance: 250,
   supportsOffsetCorrection: true,
   trackAverageRenderTimeForOffsetProjection: false,
-  invertedTransformStyle: { transform: [{ scaleY: -1 }] },
-  invertedTransformStyleHorizontal: { transform: [{ scaleX: -1 }] },
-};
-const getCellContainerPlatformStyles = (
-  inverted: boolean,
-  parentProps: { x: number; y: number; isHorizontal?: boolean }
-): { transform: string; WebkitTransform: string } | undefined => {
-  return undefined;
 };
 
-const getItemAnimator = (): BaseItemAnimator | undefined => {
-  return undefined;
-};
-
-const getFooterContainer = (): React.ComponentClass | undefined => {
-  return undefined;
-};
-
-export {
-  PlatformConfig,
-  getCellContainerPlatformStyles,
-  getItemAnimator,
-  getFooterContainer,
-};
+export { PlatformConfig };
--- src/native/config/PlatformHelper.ts
+++ src/native/config/PlatformHelper.ts
@@ -1,31 +1,7 @@
-import { BaseItemAnimator } from "recyclerlistview";
-import { DefaultJSItemAnimator } from "recyclerlistview/dist/reactnative/platform/reactnative/itemanimators/defaultjsanimator/DefaultJSItemAnimator";
-
 const PlatformConfig = {
   defaultDrawDistance: 250,
   supportsOffsetCorrection: false,
   trackAverageRenderTimeForOffsetProjection: false,
-  invertedTransformStyle: { transform: [{ scaleY: -1 }] },
-  invertedTransformStyleHorizontal: { transform: [{ scaleX: -1 }] },
-};
-const getCellContainerPlatformStyles = (
-  inverted: boolean,
-  parentProps: { x: number; y: number; isHorizontal?: boolean }
-): { transform: string; WebkitTransform: string } | undefined => {
-  return undefined;
 };
 
-const getItemAnimator = (): BaseItemAnimator | undefined => {
-  return new DefaultJSItemAnimator();
-};
-
-const getFooterContainer = (): React.ComponentClass | undefined => {
-  return undefined;
-};
-
-export {
-  PlatformConfig,
-  getCellContainerPlatformStyles,
-  getItemAnimator,
-  getFooterContainer,
-};
+export { PlatformConfig };
