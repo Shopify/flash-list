3571a487a8894d2254cba4674c2a918ea2196131
Talha Naqvi <naqvitalha@gmail.com>
2025-07-01 17:53:58 +0530

    Fix build error

--- fixture/react-native/index.js
+++ fixture/react-native/index.js
@@ -3,12 +3,9 @@
  */
 
 import { AppRegistry, I18nManager } from "react-native";
-import { enableNewCore } from "@shopify/flash-list/dist/enableNewCore";
 
 import App from "./src/App";
 import { name as appName } from "./app.json";
-
-enableNewCore(true);
 
 I18nManager.forceRTL(false);
 
