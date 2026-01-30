99ff4d8c8bb6c1653169ec5e436f82f5cacdce3c
Talha Naqvi <naqvitalha@gmail.com>
2026-01-30 10:08:02 -0800

    Unmount sticky container behind version check (#2076)

--- src/recyclerview/components/StickyHeaders.tsx
+++ src/recyclerview/components/StickyHeaders.tsx
@@ -15,6 +15,7 @@ import React, {
 import { Animated, NativeScrollEvent } from "react-native";
 
 import { FlashListProps } from "../..";
+import { PlatformConfig } from "../../native/config/PlatformHelper";
 import { RecyclerViewManager } from "../RecyclerViewManager";
 import { ViewHolder } from "../ViewHolder";
 
@@ -228,7 +229,7 @@ export const StickyHeaders = <TItem,>({
     stickyHeaderOffset,
   ]);
 
-  if (currentStickyIndex === -1) {
+  if (PlatformConfig.isRN083OrAbove && currentStickyIndex === -1) {
     return null;
   }
 
